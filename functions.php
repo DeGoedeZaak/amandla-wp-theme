c3a062b3ff84cc22c6fc154863d8f3f01de04c05
Varun Patel <info@corewaysolution.com>
2019-08-12 18:40:20 +0530

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,409 @@
+<?php
+/**
+ * Timber starter-theme
+ * https://github.com/timber/starter-theme
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since   Timber 0.1
+ */
+
+/**
+ * If you are installing Timber as a Composer dependency in your theme, you'll need this block
+ * to load your dependencies and initialize Timber. If you are using Timber via the WordPress.org
+ * plug-in, you can safely delete this block. 
+ */
+$composer_autoload = __DIR__ . '/vendor/autoload.php';
+if ( file_exists($composer_autoload) ) {
+	require_once( $composer_autoload );
+	$timber = new Timber\Timber();
+}
+
+/**
+ * This ensures that Timber is loaded and available as a PHP class.
+ * If not, it gives an error message to help direct developers on where to activate
+ */
+if ( ! class_exists( 'Timber' ) ) {
+
+	add_action( 'admin_notices', function() {
+		echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
+	});
+
+	add_filter('template_include', function( $template ) {
+		return get_stylesheet_directory() . '/static/no-timber.html';
+	});
+	return;
+}
+
+/**
+ * Sets the directories (inside your theme) to find .twig files
+ */
+Timber::$dirname = array( 'templates', 'views' );
+
+/**
+ * By default, Timber does NOT autoescape values. Want to enable Twig's autoescape?
+ * No prob! Just set this value to true
+ */
+Timber::$autoescape = false;
+
+
+/**
+ * We're going to configure our theme inside of a subclass of Timber\Site
+ * You can move this to its own file and include here via php's include("MySite.php")
+ */
+class StarterSite extends Timber\Site {
+	/** Add timber support. */
+	public function __construct() {
+		add_action( 'after_setup_theme', array( $this, 'theme_supports' ) );
+		add_filter( 'timber/context', array( $this, 'add_to_context' ) );
+		add_filter( 'timber/twig', array( $this, 'add_to_twig' ) );
+		add_action( 'init', array( $this, 'register_post_types' ) );
+		add_action( 'init', array( $this, 'register_taxonomies' ) );
+		parent::__construct();
+	}
+	/** This is where you can register custom post types. */
+	public function register_post_types() {
+
+	}
+	/** This is where you can register custom taxonomies. */
+	public function register_taxonomies() {
+
+	}
+
+	/** This is where you add some context
+	 *
+	 * @param string $context context['this'] Being the Twig's {{ this }}.
+	 */
+	public function add_to_context( $context ) {
+		$context['foo'] = 'bar';
+		$context['stuff'] = 'I am a value set in your functions.php file';
+		$context['notes'] = 'These values are available everytime you call Timber::context();';
+		$context['menu'] = new Timber\Menu();
+		$context['site'] = $this;
+		return $context;
+	}
+
+	public function theme_supports() {
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
+
+		/*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
+
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+		add_theme_support( 'post-thumbnails' );
+
+		/*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		add_theme_support(
+			'html5', array(
+				'comment-form',
+				'comment-list',
+				'gallery',
+				'caption',
+			)
+		);
+
+		/*
+		 * Enable support for Post Formats.
+		 *
+		 * See: https://codex.wordpress.org/Post_Formats
+		 */
+		add_theme_support(
+			'post-formats', array(
+				'aside',
+				'image',
+				'video',
+				'quote',
+				'link',
+				'gallery',
+				'audio',
+			)
+		);
+
+		add_theme_support( 'menus' );
+	}
+
+	/** This Would return 'foo bar!'.
+	 *
+	 * @param string $text being 'foo', then returned 'foo bar!'.
+	 */
+	public function myfoo( $text ) {
+		$text .= ' bar!';
+		return $text;
+	}
+
+	/** This is where you can add your own functions to twig.
+	 *
+	 * @param string $twig get extension.
+	 */
+	public function add_to_twig( $twig ) {
+		$twig->addExtension( new Twig_Extension_StringLoader() );
+		$twig->addFilter( new Twig_SimpleFilter( 'myfoo', array( $this, 'myfoo' ) ) );
+		return $twig;
+	}
+	
+
+}
+
+new StarterSite();
+
+
+
+
+
+
+
+/*********************************************************** customizer *****************************************************************************/
+
+function tcx_register_theme_customizer( $wp_customize ) {
+ 
+ 	$wp_customize->add_setting('banner_title', array(
+	 'default'        => '',
+	 ));
+	
+	
+	$wp_customize->add_control( 'banner_title', array(
+    'label' => __( 'Bnnner Heading Here' ),
+    'section' => 'header_image',
+    'type' => 'text',
+	)  );
+    $wp_customize->add_section('profile_settings_section', array(
+	  'title'          => 'Logo Image section'
+	 ));
+	//adding setting for footer text area
+	$wp_customize->add_setting('logo_image_c', array(
+	 'default'        => '',
+	 ));
+	
+	
+	$wp_customize->add_control( new WP_Customize_Media_Control( $wp_customize, 'logo_image_c', array(
+    'label' => __( 'Logo Image Here' ),
+    'section' => 'profile_settings_section',
+    'mime_type' => 'image',
+) ) );
+
+$wp_customize->add_section('page_heading_section', array(
+	  'title'          => 'Page Heading section'
+	 ));
+	//adding setting for footer text area
+	$wp_customize->add_setting('page_head', array(
+	 'default'        => '',
+	 ));
+	
+	
+	$wp_customize->add_control( 'page_head', array(
+    'label' => __( 'Page Heading Here' ),
+    'section' => 'page_heading_section',
+    'type' => 'text',
+)  );
+
+$wp_customize->add_section('video_section', array(
+	  'title'          => 'Video section'
+	 ));
+	//adding setting for footer text area
+	/*$wp_customize->add_setting('video', array(
+	 'default'        => '',
+	 ));*/
+	
+	
+	/*$wp_customize->add_control( new WP_Customize_Media_Control( $wp_customize, 'video', array(
+    'label' => __( 'Video Here' ),
+    'section' => 'video_section',
+    'mime_type' => 'Video',
+) ) );*/
+	$wp_customize->add_setting( 'themeslug_url_setting_id', array(
+	  'capability' => 'edit_theme_options',
+	  'sanitize_callback' => 'themeslug_sanitize_url',
+	) );
+	
+	$wp_customize->add_control( 'themeslug_url_setting_id', array(
+	  'type' => 'url',
+	  'section' => 'video_section', // Add a default or your own section
+	  'label' => __( 'viemo Video URL' ),
+	  'description' => __( 'This is a video url input.' ),
+	  'input_attrs' => array(
+		'placeholder' => __( 'http://www.google.com' ),
+	  ),
+	) );
+	
+	function themeslug_sanitize_url( $url ) {
+	  return esc_url_raw( $url );
+	}
+
+
+	$wp_customize->add_section('content_section', array(
+	  'title'          => 'Content section',
+	 ));
+	$wp_customize->add_setting('content_title', array(
+	 'default'        => '',
+	 ));
+	 $wp_customize->add_setting('content', array(
+	 'default'        => '',
+	 ));
+	
+	
+	$wp_customize->add_control('content_title', array(
+    'label' => __( 'Content Title Here' ),
+    'section' => 'video_section',
+    'type'  =>'text'
+	
+	)  );
+
+	$wp_customize->add_control('content', array(
+		'label' => __( 'Content Here' ),
+		'section' => 'video_section',
+		'type'  =>'textarea'
+		
+	)  );
+
+
+
+
+class Multi_Input_Custom_control extends WP_Customize_Control{
+	public $type = 'multi_input';
+	public function enqueue(){
+		wp_enqueue_script( 'custom_controls', get_template_directory_uri().'/js/theme-customizer-new.js', array( 'jquery' ),'', true );
+		
+	}
+	public function render_content(){
+		?>
+		<label class="customize_multi_input">
+			<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+			<input type="hidden" id="<?php echo esc_attr($this->id); ?>" name="<?php echo esc_attr($this->id); ?>" value="<?php echo esc_attr($this->value()); ?>" class="customize_multi_value_field" data-customize-setting-link="<?php echo esc_attr($this->id); ?>"/>
+			<div class="customize_multi_fields">
+				<div class="set">
+					<input type="text" value="" class="customize_multi_single_field" style="width:90%; display:inline-block; margin-bottom:15px;"/>
+					<a href="#" class="customize_multi_remove_field">X</a>
+				</div>
+			</div>
+			<a href="#" class="button button-primary customize_multi_add_field"><?php esc_attr_e('Add More', 'mytheme') ?></a>
+		</label>
+		<?php
+	}
+}
+
+//namespace tad\customizer\controls;
+
+if (!class_exists('\WP_Customize_Image_Control')) {
+    return null;
+}
+class MultiImageControl extends \WP_Customize_Image_Control
+{
+    public $type = 'multi-image';
+
+    public function enqueue()
+    {
+        wp_enqueue_media();
+		wp_enqueue_script( 'custom_controls', get_template_directory_uri().'/js/theme-customizer-new.js', array( 'jquery' ),'', true );
+        // enqueue the script
+        // enqueue the style
+    }
+
+    public function render_content()
+    {
+        // get the set values if any
+		$imageSrcs = array();
+        $imageSrcs = explode(",",$this->value());
+			
+		
+        if (is_null($imageSrcs)) {
+            $imageSrcs = array();
+        }
+        // render the title
+        $this->theTitle();
+		
+       $this->theUploadedImages($imageSrcs);
+	    $this->theButtons();
+	   
+    }
+    protected function theTitle()
+    {
+        ?>
+        <label>
+            <span class="customize-control-title">
+                <?php echo esc_html($this->label); ?> 
+            </span> 
+        </label>
+        <?php
+    }
+    
+	
+
+	 
+}
+/**
+Multiple input field
+**/
+$wp_customize->add_setting('multi_field', array(
+	'default'           => '',
+	
+));
+
+
+
+	$wp_customize->add_section('copyright_text', array(
+	  'title'          => 'Copyright Text'
+	 ));
+	//adding setting for footer text area
+	$wp_customize->add_setting('site_info', array(
+	 'default'        => '',
+	 ));
+	
+	
+	$wp_customize->add_control( 'site_info', array(
+    'label' => __( 'Copy Right Text' ),
+    'section' => 'copyright_text',
+    'type' => 'text',
+	));
+
+$wp_customize->remove_section( 'colors' );
+$wp_customize->remove_section( 'background_image' );
+$wp_customize->remove_section( 'static_front_page' );
+$wp_customize->remove_section( 'custom_css' );
+ 
+}
+add_action( 'customize_register', 'tcx_register_theme_customizer' );
+
+/**
+ * Add a sidebar.
+ */
+function wpdocs_theme_slug_widgets_init() {
+    register_sidebar( array(
+        'name'          => __( 'Main Sidebar', 'textdomain' ),
+        'id'            => 'sidebar-1',
+        'description'   => __( 'Widgets in this area will be shown on all posts and pages.', 'textdomain' ),
+        'before_widget' => '<li id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</li>',
+        'before_title'  => '<h2 class="widgettitle">',
+        'after_title'   => '</h2>',
+    ) );
+	
+	register_sidebar( array(
+        'name'          => __( 'Main Sidebar 3', 'textdomain' ),
+        'id'            => 'sidebar-2',
+        'description'   => __( 'Widgets in this area will be shown on all posts and pages.', 'textdomain' ),
+        'before_widget' => '<li id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</li>',
+        'before_title'  => '<h2 class="widgettitle">',
+        'after_title'   => '</h2>',
+    ) );
+}
+add_action( 'widgets_init', 'wpdocs_theme_slug_widgets_init' );
+
+
+
+
+
+
---
+++ page-home.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template Name: Home Page
+ * Description: A Page Template with a darker design.
+ */
+
+// Code to display Page goes here...
+
+
+
+$context = Timber::context();
+
+ $people_count = 268289;	
+ $home_top_title_c = get_field('home_top_title');	
+ $people_count_add = $string = str_replace('{{}}',$people_count, $home_top_title_c);
+	
+$context['home_top_title'] =  $people_count_add;
+$context['home_top_image'] =  get_field('home_top_image');
+$context['home_top_description'] =  get_field('home_top_description');
+
+$home_top_description_mobile_r = get_field('home_top_description_mobile');	
+$home_top_description_mobile_t = $string = str_replace('{{}}',$people_count, $home_top_description_mobile_r);
+
+$context['home_top_description_mobile'] =  $home_top_description_mobile_t;
+$context['latest_campaigns_title'] =  get_field('latest_campaigns_title');
+$context['campaign_text_title'] =  get_field('campaign_text_title');
+$context['campaign_btn_text'] =  get_field('campaign_btn_text');
+$context['campaign_btn_link'] =  get_field('campaign_btn_link');
+$context['campaign_btn_text_mobile'] =  get_field('campaign_btn_text_mobile');
+$context['campaign_btn_link_mobile'] =  get_field('campaign_btn_link_mobile');
+$context['home_donation_text'] =  get_field('home_donation_text');
+$context['home_donation_btn_title'] =  get_field('home_donation_btn_title');
+$context['home_donation_link'] =  get_field('home_donation_link');
+$context['latest_news_title'] =  get_field('latest_news_title');
+
+
+$timber_post = new Timber\Post();
+$context['post'] = $timber_post;
+Timber::render( array('page-' . $timber_post->post_name . '.twig', 'page.twig' ), $context );
+
+
+
